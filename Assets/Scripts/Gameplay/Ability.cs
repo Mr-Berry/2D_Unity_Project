f36eda954fe44c758006090e218c8246a1550751
Jason Berry <mrberry1990@gmail.com>
2017-11-02 19:19:01 -0400

    Added ability holders to Inventory editor in menu

--- Assets/Scripts/Gameplay/Ability.cs
+++ Assets/Scripts/Gameplay/Ability.cs
@@ -7,6 +7,7 @@ public class Ability : MonoBehaviour {
 
 	public Sprite[] m_sprites;
 	private Image m_img;
+	public bool m_inPlayMode = true;
 	protected short m_type { get; set; }
 
 	void Start() {
@@ -20,23 +21,25 @@ public class Ability : MonoBehaviour {
 	}
 
 	public void Activate() {
-		//click it and it does this
-		switch (m_type) {
-			case 0:
-				Debug.Log("Default type of nothing");
-			break;
-			case 1:
-				//Fireballs here
-			break;
-			case 2:
-				//Summon here
-			break;
-			case 3:
-				//Shield here
-			break;
-			default:
-				Debug.Log("Unknown type");
-			break;
+		if (m_inPlayMode) {
+			//click it and it does this
+			switch (m_type) {
+				case 0:
+					Debug.Log("Default type of nothing");
+				break;
+				case 1:
+					//Fireballs here
+				break;
+				case 2:
+					//Summon here
+				break;
+				case 3:
+					//Shield here
+				break;
+				default:
+					Debug.Log("Unknown type");
+				break;
+			}
 		}
 	}
 
