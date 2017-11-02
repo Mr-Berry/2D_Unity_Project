367e3475f2ce5997bdc833285fd7b1770849b03d
Jason Berry <mrberry1990@gmail.com>
2017-11-01 21:05:49 -0400

    edited damage script

--- Assets/Scripts/Gameplay/Damage.cs
+++ Assets/Scripts/Gameplay/Damage.cs
@@ -7,6 +7,7 @@ public class Damage : MonoBehaviour {
 	// more than two types that deal damage do this --> public short m_type
 	public short m_damage;
 	public float m_attackRate;
+	public bool m_isProjectile;
 	private float m_timer;
 	private Collider m_clr;
 	private Health m_target;
@@ -21,7 +22,7 @@ public class Damage : MonoBehaviour {
 			if (m_timer <= 0) {
 				m_target.TakeDamage(m_damage);
 				m_timer = m_attackRate;
-				if (tag == "projectile") {
+				if (m_isProjectile) {
 					// death particle here
 					// GetComponent<ParticleSystem>().Play;
 					Destroy(this);
@@ -35,8 +36,4 @@ public class Damage : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other) {
 		m_target = other.gameObject.GetComponent<Health>();
 	}
-
-	// void OnTriggerEnter2D(Collider2D other) {
-	// 	m_target = other.gameObject.GetComponent<Health>();
-	// }
 }
