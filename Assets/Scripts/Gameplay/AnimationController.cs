3620337f234d2253f335c0a6fc3a54a15d978284
Jason Berry <mrberry1990@gmail.com>
2017-11-03 14:50:23 -0400

    added new summon, dragonwarrior, added animation script and setup animator

---
+++ Assets/Scripts/Gameplay/AnimationController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	private Damage m_damage;
+	private Health m_health;
+	private Rigidbody2D m_rb;
+	private Animator m_anim;
+	void Start () {
+		m_anim = GetComponent<Animator>();
+		m_damage = GetComponent<Damage>();
+		m_health = GetComponent<Health>();
+		m_rb = GetComponent<Rigidbody2D>();
+	}	
+	
+	void Update () {
+		HandleDeathAnim();
+		HandleAttackAnim();
+		HandleMovementAnim();
+	}
+
+	private void HandleAttackAnim() {
+
+	}
+
+	private void HandleDeathAnim() {
+
+	}
+
+	private void HandleMovementAnim() {
+		if (Mathf.Abs(m_rb.velocity.x) > 0) {
+			m_anim.SetBool("isMoving", true);
+		} else {
+			m_anim.SetBool("isMoving", false);
+		}
+	}
+}
