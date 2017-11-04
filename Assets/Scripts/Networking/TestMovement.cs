1ecd0804fccdc698d257bc1e14c32ff2769e7da1
Josh Brackett <joshua.brackett@hotmail.ca>
2017-11-03 20:54:29 -0400

    added network scene and got fireballs/playermovement to work over network

---
+++ Assets/Scripts/Networking/TestMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestMovement : NetworkBehaviour {
+
+	public int m_movementSpeed = 5;
+	public Rigidbody2D m_rb;
+	public GameObject m_fireball;
+	private bool m_isLocal;
+
+	void Start() {
+		GetComponent<TestMovement>().enabled = true;
+		m_rb = GetComponent<Rigidbody2D>();
+	}
+ 
+	void Update() {
+		if(isLocalPlayer) {
+			if(Input.GetKey(KeyCode.A)) {
+				m_rb.velocity = Vector3.left * m_movementSpeed;
+			}
+			if(Input.GetKeyUp(KeyCode.A)) {
+				m_rb.velocity = Vector3.zero;
+			}
+			if(Input.GetKey(KeyCode.D)) {
+				m_rb.velocity = Vector3.right * m_movementSpeed;
+			}
+			if(Input.GetKeyUp(KeyCode.D)) {
+				m_rb.velocity = Vector3.zero;
+			}
+			if(Input.GetKeyDown(KeyCode.Space)) {
+				CmdSpawnFireball();
+			}
+		}
+	}
+
+	[Command]
+	public void CmdSpawnFireball() {
+		GameObject tmp = Instantiate(m_fireball, transform.position, Quaternion.identity);
+		NetworkServer.Spawn(tmp);
+	}
+}
